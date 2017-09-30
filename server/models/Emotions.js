69f2093860c5875abd448427c664c9c3efc82ed9
hsarukhanyan <hsarukhanyan@macadamian.com>
2017-09-30 13:46:21 +0400

    model is integrated with routs

--- server/models/Emotions.js
+++ server/models/Emotions.js
@@ -2,12 +2,16 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-let EmotionsSchema = new Schema({});
+let EmotionsSchema = new Schema({
+  "faceRectangle": {type: Object},
+  "scores": {type: Object},
+  "timeStamp": {type: Date, required: true}
+});
 
 let EmotionsModel = mongoose.model('emotions', EmotionsSchema);
 
 function save(data) {
-  return EmotionsModel.save(data)
+  return new EmotionsModel(JSON.parse(data)).save();
 }
 
 function findAll() {
