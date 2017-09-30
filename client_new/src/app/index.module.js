8857d9d4fde00017780bc4bc1791e23e1eba1dd4
Hayk Sargsyan <hsargsyan@macadamian.com>
2017-09-30 19:18:07 +0400

    statistics init ui

--- client_new/src/app/index.module.js
+++ client_new/src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('app', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+    .module('app', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'chart.js']);
 
 })();
